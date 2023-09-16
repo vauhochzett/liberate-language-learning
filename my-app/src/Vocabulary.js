718d8f297f04097c05839b10c3ed25f38cc983e0
Khiem Ton-That <k.ton@thalia.de>
2023-09-17 00:36:24 +0200

    Acc ID Vocabulary and show certificate

--- my-app/src/Vocabulary.js
+++ my-app/src/Vocabulary.js
@@ -1,5 +1,6 @@
 // Vocabulary.js
 import React, { useState } from "react";
+import Cookies from "js-cookie";
 
 const Vocabulary = ({ word, translation }) => {
   const [correct, setCorrect] = useState(null);
@@ -18,13 +19,15 @@ const Vocabulary = ({ word, translation }) => {
       body: JSON.stringify({
         OriginalString: word,
         TranslatedString: inputValue,
+        AccId: Cookies.get("accId"),
+        Language: "de",
       }),
     });
 
     if (response.ok) {
       const data = await response.json(); // Parse the JSON response
-      setCorrect(data?.correct); // Set the 'correct' state based on the response
-      setCertificate(data?.certificate); // Set the 'certificate' state based on the response
+      setCorrect(data?.Correct); // Set the 'correct' state based on the response
+      setCertificate(data?.Certificate); // Set the 'certificate' state based on the response
     } else {
       console.error("Failed to mark word as correct.");
       setCorrect(false);
